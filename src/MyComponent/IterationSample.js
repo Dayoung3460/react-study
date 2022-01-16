02eca9e708e61ae382b3341c20ca966c8350eb52
Dayoung Kim <kimdayoung771@gmail.com>
2022-01-16 18:55:03 +0900

    add list

--- src/MyComponent/IterationSample.js
+++ src/MyComponent/IterationSample.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const MyComponent = () => {
-  const names = ['snowman', 'ice', 'snow', 'wind']
-  const nameList = names.map(name => {
-    return <li key={Math.random()}>{name}</li>
+  const [name, setNames] = useState([
+    {id: 1, text: 'snowman'},
+    {id: 2, text: 'ice'},
+    {id: 3, text: 'snow'},
+    {id: 4, text: 'wind'}
+  ])
+
+  const [input, setInput] = useState('')
+  const [nextId, setNextId] = useState(5)
+  const nameList = name.map((name, idx) => {
+    return <li key={name.id}>{name.text}</li>
   })
+
+  const onChange = e => {
+    setInput(e.target.value)
+  }
+
+  const onClick = () => {
+    setNames([
+      ...name,
+      { id: nextId, text: input }
+    ])
+    setNextId(nextId + 1)
+    setInput('')
+  }
 
   return (
     <div>
-      {nameList}
+      <input
+        value={input}
+        onChange={onChange}
+      />
+      <button onClick={onClick}>Add</button>
+      <ul>{nameList}</ul>
     </div>
   );
 };
