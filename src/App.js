407e8a9c6eb41655b7bfef0f34420d029f5c2cfc
Dayoung Kim <kimdayoung771@gmail.com>
2022-01-16 18:32:45 +0900

    comp ref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import ValidationSample from "./MyComponent/ValidationSample";
+import ScrollBox from "./MyComponent/ScrollBox";
+import {Component} from "react";
 
-function App() {
-  return (
-    <ValidationSample/>
-  );
+class App extends Component{
+  render() {
+    return (
+      <>
+        <ScrollBox ref={ref => this.scrollBox = ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>Go Bottom</button>
+      </>
+    )
+  }
 }
 
 export default App;
