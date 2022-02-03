81d774c724a32c704d040e63a277f14868d1050c
Dayoung Kim <kimdayoung771@gmail.com>
2022-02-03 21:51:33 +0900

    use 'useReducer' for to-do project

--- src/MyComponent/UseReducer.js
+++ src/MyComponent/UseReducer.js
@@ -1,35 +1,56 @@
 import React, {useReducer, useState} from 'react';
+import Todo from "./Todo";
 
-function reducer (state, action) {
-  if(action.type === 'increment') {
-    return { count: state.count + 1 }
-  } else if (action.type === 'decrement') {
-    return { count: state.count - 1 }
-  } else {
-    return state
+const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle_todo',
+  DELETE_TODO: 'delete_todo'
+}
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if(todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo
+    })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => (
+        todo.id !== action.payload.id
+      ))
   }
 }
 
+const newTodo = (name) => {
+  return { id: Math.random(), name: name, complete: false }
+}
+
 const UseReducer = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('')
 
-  // const [count, setCount] = useState(0)
-
-  const decrement = () => {
-    dispatch({ type: 'decrement' })
-  }
-
-  const increment = () => {
-    dispatch({ type: 'increment' })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } })
+    setName('')
   }
 
   return (
     <div>
-      <button onClick={decrement}>-</button>
-      <span>{ state }</span>
-      <button onClick={increment}>+</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={e =>
+          setName(e.target.value)}/>
+      </form>
+      { todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+      }) }
     </div>
   );
 };
 
 export default UseReducer;
+export { ACTIONS }
